dbc7c0e94bbc6ed6e68709689b455c260e79cf91
priyanka <priyankamail0318@gmail.com>
2024-10-12 23:04:36 -0400

    Initial Commit.

---
+++ src/test/java/com/appl/kitchensinkserver/new_kitchensinkserver/NewKitchensinkserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appl.kitchensinkserver.new_kitchensinkserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewKitchensinkserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
